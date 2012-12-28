8a306277b95e497a2df1a676414c1369ea50713f
LightGuard <lightguard.jp@gmail.com>
2012-12-28 16:11:04 -0700

    basic site

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,16 @@
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new( '/tech' ) 
+  extension Awestruct::Extensions::Posts.new( '/religious' ) 
+  extension Awestruct::Extensions::Posts.new( '/misc' ) 
+  extension Awestruct::Extensions::Disqus.new()
+  extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new(:tech, '/tech/feed.atom')
+  extension Awestruct::Extensions::Atomizer.new(:misc, '/misc/feed.atom')
+  extension Awestruct::Extensions::Atomizer.new(:religious, '/religious/feed.atom')
+
+  # TODO: Tags
+
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
