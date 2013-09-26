e7746b557a47b1b5b34ad18330607096f9b00abb
LightGuard <lightguard.jp@gmail.com>
2013-09-26 13:22:57 -0600

    Adding in presentations

--- Gemfile
+++ Gemfile
@@ -1,14 +1,13 @@
 source 'https://rubygems.org'
 
-#gem "awestruct", :path => '/home/jporter/projects/ruby/awestruct'
+gem "awestruct", :path => '/home/jporter/projects/ruby/awestruct'
 #gem "awestruct", :github => 'awestruct'
-gem "awestruct", '~> 0.5'
-gem "html_truncator"
+#gem "awestruct", '~> 0.5'
 gem 'rb-inotify', '~> 0.9.0'
 gem 'tilt', '~> 1.4'
 gem 'puma'
 gem 'markaby'
-gem 'asciidoctor', '~> 0.1'
+gem 'asciidoctor', '~> 0.1.4'
 gem 'slim', '~> 2.0'
 gem 'kramdown', :platforms => :jruby
 gem 'rdiscount', :platforms => :ruby
@@ -16,6 +15,4 @@ gem 'less', '~> 2.2.2'
 gem "therubyracer", '~> 0.10.0', :platforms => :ruby
 gem "therubyrhino", :platforms => :jruby
 gem 'htmlcompressor', '~> 0.0.5'
-gem 'uglifier', '~> 1.3.0'
-
-gem 'ruby-prof'
+gem "html_truncator"
--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,16 +7,16 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/religious', :religious) 
   extension Awestruct::Extensions::Posts.new('/misc', :misc) 
   extension Awestruct::Extensions::Disqus.new()
-  extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::Atomizer.new(:tech, '/tech/feed.atom')
-  extension Awestruct::Extensions::Atomizer.new(:misc, '/misc/feed.atom')
-  extension Awestruct::Extensions::Atomizer.new(:religious, '/religious/feed.atom')
+  extension Awestruct::Extensions::Indexifier.new()
+  #extension Awestruct::Extensions::Atomizer.new(:tech, '/tech/feed.atom')
+  #extension Awestruct::Extensions::Atomizer.new(:misc, '/misc/feed.atom')
+  #extension Awestruct::Extensions::Atomizer.new(:religious, '/religious/feed.atom')
 
   # TODO: Tags
 
   helper Awestruct::Extensions::GoogleAnalytics
   helper Awestruct::Extensions::Truncate
 
-  transformer Awestruct::Extensions::Minify.new
+  #transformer Awestruct::Extensions::Minify.new
 end
 
